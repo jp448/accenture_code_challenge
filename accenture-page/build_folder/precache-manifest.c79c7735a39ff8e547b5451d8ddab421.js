3efcb5e9fd6c9bc59d07aefe1f771c3b43ece399
Jocelyn Proctor <jocelynkproctor@gmail.com>
2020-10-22 22:56:09 +0200

    build app

---
+++ accenture-page/build_folder/precache-manifest.c79c7735a39ff8e547b5451d8ddab421.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb52f22774daa11611298368aca76c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83a8dc5c4023460a50a4",
+    "url": "/static/css/main.3bcb673f.chunk.css"
+  },
+  {
+    "revision": "0ce92839073a118517ee",
+    "url": "/static/js/2.d05e963f.chunk.js"
+  },
+  {
+    "revision": "cca292a99906e269161e23f456b6adca",
+    "url": "/static/js/2.d05e963f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a8dc5c4023460a50a4",
+    "url": "/static/js/main.206c8a64.chunk.js"
+  },
+  {
+    "revision": "0fd53fa7bf93345de0bd",
+    "url": "/static/js/runtime-main.d85dc49b.js"
+  },
+  {
+    "revision": "90812c198e389b61bcf8637325968f05",
+    "url": "/static/media/accenture_banner.90812c19.jpg"
+  },
+  {
+    "revision": "81034b722460aa5249ae888996126a45",
+    "url": "/static/media/adapters.81034b72.jpeg"
+  },
+  {
+    "revision": "e32da60e2c73983d4aea592e72c3b178",
+    "url": "/static/media/collaborators.e32da60e.jpg"
+  },
+  {
+    "revision": "53e1562591bc9658725d6bffa5c2155f",
+    "url": "/static/media/diversity.53e15625.jpeg"
+  },
+  {
+    "revision": "6bc5dcaf5168b062849c1f4e39c6d50f",
+    "url": "/static/media/inclusion.6bc5dcaf.jpg"
+  },
+  {
+    "revision": "b9de8be898e01450ab05f66ae38ab266",
+    "url": "/static/media/innovators.b9de8be8.jpg"
+  },
+  {
+    "revision": "410776c2e6f4987d145eb27dc5b48ef4",
+    "url": "/static/media/integrity.410776c2.jpg"
+  },
+  {
+    "revision": "894ebdab8e710d55930a334d83c8f706",
+    "url": "/static/media/robot.894ebdab.jpg"
+  },
+  {
+    "revision": "c41ead07b5703023cde6bc2c9876a82a",
+    "url": "/static/media/strategy.c41ead07.jpg"
+  },
+  {
+    "revision": "d5b0dc6fa70cb6087fdd4b2d614c675b",
+    "url": "/static/media/transform.d5b0dc6f.jpg"
+  }
+]);
